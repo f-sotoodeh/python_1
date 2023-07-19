51cebed8016f1bdc6e5680650a3a5c4463ed3836
Farhad <so.to.the@gmail.com>
2023-07-19 23:47:38 +0330

    update lesson 17

--- 17/if.py
+++ 17/if.py
@@ -19,17 +19,17 @@
 n = int(input('Enter a number: '))
 if n == 0:
     print('shanbe')
-if n == 1:
+elif n == 1:
     print('yek shanbe')
-if n == 2:
+elif n == 2:
     print('do shanbe')
-if n == 3:
+elif n == 3:
     print('se shanbe')
-if n == 4:
+elif n == 4:
     print('char shanbe')
-if n == 5:
+elif n == 5:
     print('panj shanbe')
-if n == 6:
+elif n == 6:
     print('jome')
 # else:
 #     print('wrong!')
