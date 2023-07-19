86fbe51cd7d79c6dd3fc310e249a78f571f2087c
Farhad <so.to.the@gmail.com>
2023-07-19 23:41:41 +0330

    add lesson 17

---
+++ 17/if.py
@@ -0,0 +1,37 @@
+# n = int(input('Enter a number: '))
+# if n < 10:
+#     print('This is a one-digit number.')
+# elif n < 100:
+#     print('This is a two-digit number.')
+# else:
+#     print('This is a great number.')
+# print('Done!')
+
+
+
+# n = int(input('Enter a number: '))
+# if n % 2 == 0:
+#     print('Even')
+# else:
+#     print('Odd')
+
+
+n = int(input('Enter a number: '))
+if n == 0:
+    print('shanbe')
+if n == 1:
+    print('yek shanbe')
+if n == 2:
+    print('do shanbe')
+if n == 3:
+    print('se shanbe')
+if n == 4:
+    print('char shanbe')
+if n == 5:
+    print('panj shanbe')
+if n == 6:
+    print('jome')
+# else:
+#     print('wrong!')
+
+
