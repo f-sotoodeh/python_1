e03f9d1d4549982caf214a9eb22b9e2696d5c85b
Farhad <so.to.the@gmail.com>
2023-07-20 01:48:21 +0330

    add lesson 18

---
+++ 18/while.py
@@ -0,0 +1,23 @@
+# a = 1
+# while a <= 20:
+#     print(a)
+#     a += 1
+
+
+# s = 0
+# a = int(input('? '))
+# while a > 0:
+#     s += a
+#     a = int(input('? '))
+# print(s)
+
+
+s = 0
+while True:
+    a = int(input('? '))
+    if a <= 0:
+        break
+    if a % 3 == 0:
+        continue
+    s += a
+print(s)
