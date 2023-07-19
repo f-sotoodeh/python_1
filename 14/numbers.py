81e56fa3a70d0449278418bddabab3d842d08016
Farhad <so.to.the@gmail.com>
2023-07-19 22:04:50 +0330

    add lesson 14

---
+++ 14/numbers.py
@@ -0,0 +1,24 @@
+
+# I want to add two numbers
+# a = 2  # first variable
+# b = 5
+# c = a + b
+# print(c)
+
+
+# print(11+2)
+# print(11-2)
+# print(11*2)
+# print(11/2)
+# print(11//2)
+# print(11%2)
+# print(11**2)
+# a) 5
+# b) 5.0
+
+
+# print('Hello'+'4')
+
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
+print(a+b)
