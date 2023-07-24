dc4c789674aacee1dd39ef7dbad75bff6bab5b09
Farhad <so.to.the@gmail.com>
2023-07-25 01:24:30 +0330

    add lesson 24

---
+++ 24/list.py
@@ -0,0 +1,31 @@
+
+
+# l = [2, 3.5, False, [], ['Hello', True], None]
+
+# print(l[0])
+# print(l[-6])
+# print(l[-1])
+# print(l[4][0])
+# print(l[-2][0])
+# print(l[-2][-2])
+
+
+#    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+# l = [9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6]
+
+
+# print(l[4:11])
+# print(l[4:11:2])
+# print(l[:11:2])
+# print(l[::3])
+# print(l[::-1])
+# print(l[-3:5:-1])
+
+# print(l[20])
+
+
+print(['Even', 'Odd'][int(input('? '))%2])
+
+
+
+
