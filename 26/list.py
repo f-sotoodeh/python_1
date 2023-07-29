d9ecbfe2ae3923391eb7c516bb18fe5f38b4ae7b
Farhad <so.to.the@gmail.com>
2023-07-29 16:52:43 +0330

    add lesson 26 & lesson 27

---
+++ 26/list.py
@@ -0,0 +1,25 @@
+
+
+l = [8,6,3,9,8,2,5,8]
+
+# print(len(l))
+# print(sum(l))
+
+# print(min(l))
+# print(max(l))
+
+# l2 = sorted(l)
+# print(l2)
+# print(sorted(l))
+# print(l)
+
+# print(l.sort())
+# print(l)
+
+# print(l.index(8))
+# print(l.count(8))
+
+for idx, val in enumerate(l):
+    print(idx, val)
+
+    
