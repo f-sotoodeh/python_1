f3cbf9291df2a5117bf05632fd84f5b3e8a33cb3
Farhad <so.to.the@gmail.com>
2023-08-07 23:25:07 +0330

    add lesson 33

---
+++ 33/string.py
@@ -0,0 +1,32 @@
+
+
+
+# a = 12
+# b = 34
+# """
+# askjldhkas
+# lkfdhkf
+# lkdflkjf
+# lkdflds
+# """
+# c = a + b
+# s = '''
+# abcd
+# efgh
+# '''
+# print(c)
+
+# D:
+# ed.txtder
+
+from time import sleep
+from datetime import datetime
+
+while True:
+    t = datetime.now().strftime('%H:%M:%S')
+    print(t, end='\r')
+    sleep(1)
+
+
+
+
